037f99fe5a733ee7a806ef4fdbd7118f7128db02
SemanticBeeng <nick@semanticbeeng.com>
2016-10-13 01:32:35 -0700

    first search and field extraction

--- elastic4s-examples/src/main/scala/com/fastfish/ESRemoteTest.java
+++ elastic4s-examples/src/main/scala/com/fastfish/ESRemoteTest.java
@@ -1,21 +1,23 @@
 package com.fastfish;
 
 import java.net.InetSocketAddress;
+import java.util.Map;
 
-import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.client.Client;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
+import org.elasticsearch.index.query.QueryBuilders;
+
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.shield.ShieldPlugin;
 
 
 public class ESRemoteTest {
 
-    private Client client;
-
-
-    public void testConnect() {
+    public void testSearch() {
 
         try {
             String clusterId = "34cccf87611bad4eedae9127b94759ab";
@@ -31,8 +33,21 @@ public class ESRemoteTest {
                             .put("request.headers.X-Found-Cluster", clusterId)
                             .build()).build().
                             addTransportAddress(new InetSocketTransportAddress(new InetSocketAddress(clusterId+".eu-west-1.aws.found.io", 9343)));
-            GetResponse response = client.prepareGet("fastfish", "logs","AVe3ng8KbVtTkaw8pXIm").get();
-            System.out.println("done get");
+            //GetResponse response = client.prepareGet("fastfish", "logs","AVe3ng8KbVtTkaw8pXIm").get();
+            SearchResponse response = client.prepareSearch("fastfish")
+                    // .setTypes("type1", "type2")
+                    .setSearchType(SearchType.QUERY_THEN_FETCH)
+                    .setQuery(QueryBuilders.multiMatchQuery("water", "brand_name","category_id","long_description"))
+                    .setFrom(0).setSize(60).setExplain(true)
+                    .execute()
+                    .actionGet();
+
+            System.out.println("done search" + response.getHits());
+            for (org.elasticsearch.search.SearchHit hit : response.getHits().hits()) {
+                final Map<String, Object> fields = hit.getSource();
+
+                System.out.println("Fields =" + fields);
+            }
             client.close();
         } catch ( Exception e) {
             // TODO Auto-generated catch block
@@ -49,8 +64,8 @@ public class ESRemoteTest {
 
     public static void main(String[] args) {
 
-        ESRemoteTest conn = new ESRemoteTest();
-        conn.testConnect();
+        ESRemoteTest search = new ESRemoteTest();
+        search.testSearch();
 
     }
 
